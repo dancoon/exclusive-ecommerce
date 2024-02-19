adab30081dba49d0d53c73623e36c3cc3943adb4
dancoon <dancun.ian@students.jkuat.ac.ke>
2024-02-19 19:42:14 +0300

    fix: :bug: Fix logout page

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -56,7 +56,7 @@ const page = () => {
                   </h3>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      Are you sure you want to sign out? 
+                      Are you sure you want to sign out?
                     </p>
                   </div>
                 </div>
